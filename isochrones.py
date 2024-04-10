bcb7777343c8a6e9f56f670e59f439bdf26bdd76
Jack <121068188+bopo16@users.noreply.github.com>
2024-04-10 15:26:14 +1000

    Download data

--- isochrones.py
+++ isochrones.py
@@ -1,4 +1,3 @@
-
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +5,7 @@ import networkx as nx
 import osmnx as ox
 import os
 from dotenv import load_dotenv
+from osmnx import settings
 
 # load environment variables from .env file
 load_dotenv()
@@ -14,16 +14,30 @@ load_dotenv()
 api_key = os.getenv("TRAVELTIME_API_KEY")
 application_id = os.getenv("TRAVELTIME_APPLICATION_ID")
 
-# get the polygon for Parramatta, NSW
-city = ox.geocode_to_gdf('Parramatta, NSW, Australia')
-polygon = city['geometry'].iloc[0]
 
-# get the street network within the polygon
-G = ox.graph_from_polygon(polygon, network_type='walk')
+# set data save location
+settings.data_folder = "data"
+
+# check if the data is already downloaded
+if not os.path.exists("data/city.shp") or not os.path.exists("data/graph.graphml"):
+    # get the polygon for Parramatta, NSW
+    city = ox.geocode_to_gdf("Sydney, NSW, Australia")
+    polygon = city["geometry"].iloc[0]
+
+    # get the street network within the polygon
+    G = ox.graph_from_polygon(polygon, network_type="walk")
+
+    # save the GeoDataFrame to a shapefile
+    city.to_file("data/city.shp")
+
+    # save the Graph to a GraphML file
+    ox.save_graphml(G, filepath="data/graph.graphml")
+else:
+    # load the data from the files
+    city = gpd.read_file("data/city.shp")
+    G = ox.load_graphml("data/graph.graphml")
 
 # display the objects
-fig, ax = ox.plot_graph(G, bgcolor='white', show=False, close=False)
-city.plot(ax=ax, facecolor='none', edgecolor='lightgrey')
+fig, ax = ox.plot_graph(G, bgcolor="white", show=False, close=False)
+city.plot(ax=ax, facecolor="none", edgecolor="lightgrey")
 plt.show()
-
-
